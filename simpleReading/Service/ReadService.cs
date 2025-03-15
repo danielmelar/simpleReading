b4eadd5dfb4efd0a78f2f9f9cefffca409ef2368
Daniel Melo Aguilar <danielmelo.aguilar@gmail.com>
2025-03-15 17:31:28 -0300

    feat: add read

--- simpleReading/Service/ReadService.cs
+++ simpleReading/Service/ReadService.cs
@@ -4,5 +4,10 @@ public class ReadService
 {
     public List<Read> reads = new List<Read>();
 
-    public List<Read> GetReads() => reads;   
+    public List<Read> GetReads() => reads; 
+
+    public void AddRead(Read read)
+    {
+        reads.Add(read);
+    }  
 }
