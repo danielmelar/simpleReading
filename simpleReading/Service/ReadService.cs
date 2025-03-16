de1db834067add2447a1b54e629e1a35cd8b7655
Daniel Melo Aguilar <danielmelo.aguilar@gmail.com>
2025-03-16 05:23:22 -0300

    feat: removing read

--- simpleReading/Service/ReadService.cs
+++ simpleReading/Service/ReadService.cs
@@ -6,11 +6,25 @@ public class ReadService
 
     public List<Read> GetReads() => reads; 
 
+    public Read GetReadById(int id)
+    {
+        var foundRead = reads.First(x => x.Id == id);
+        if (foundRead == null)
+            return null;
+
+        return foundRead;
+    }
+
     public void AddRead(Read read)
     {
         read.Id = CreateId();
         reads.Add(read);
     } 
+
+    public void RemoveRead(int id)
+    {
+        reads.Remove(reads.Find(x => x.Id == id));
+    }
 
     private int CreateId()
     {
