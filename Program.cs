eda274a52783b8c10a9f532096967849984b8f8d
Daniel Melo Aguilar <danielmelo.aguilar@gmail.com>
2025-03-21 03:53:53 -0300

    feat: local storage

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using simpleReading;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -10,5 +11,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
