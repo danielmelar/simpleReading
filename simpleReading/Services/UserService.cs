91b203a3582f47d9c0244c8fb67b4b4404c78e1e
Daniel Melo Aguilar <danielmelo.aguilar@gmail.com>
2025-04-16 01:37:04 -0300

    feat: hash e normalizacao de inputs

--- simpleReading/Services/UserService.cs
+++ simpleReading/Services/UserService.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using simpleReading.Context;
 using simpleReading.Interfaces;
 using simpleReading.Models;
+using static BCrypt.Net.BCrypt;
 
 namespace simpleReading.Services
 {
@@ -15,8 +16,8 @@ namespace simpleReading.Services
 
         public async Task<AuthenticationResult> GetUserByLoginCredentials(string email, string password)
         {
-            var user = await _context.User.FirstOrDefaultAsync(predicate: x => x.Email == email && x.Password == password);
-            if (user == null) return new AuthenticationResult(false, "Usuário não encontrado! Verifique o email e senha.");
+            var user = await _context.User.FirstOrDefaultAsync(predicate: x => x.Email == email.ToLower().Trim());
+            if (user == null || !VerifyPasswd(password.ToLower().Trim(), user.Password)) return new AuthenticationResult(false, "Usuário não encontrado! Verifique o email e senha.");
                 
             return new AuthenticationResult(true, "Bem vindo!", user);
         }
@@ -24,14 +25,28 @@ namespace simpleReading.Services
         public async Task<AuthenticationResult> Create(User input)
         {
             var user = await _context.User.FirstOrDefaultAsync(
-                predicate: x => x.Username == input.Username || x.Email == input.Email);
+                predicate: x => x.Username == input.Username.ToLower().Trim() || x.Email == input.Email.ToLower().Trim());
             if (user != null)
                 return new AuthenticationResult(false, "Apelido e/ou email já em uso.");
+
+            input.Password = HashPasswd(input.Password.ToLower().Trim());
             
             await _context.User.AddAsync(input);
             await _context.SaveChangesAsync();
 
             return new AuthenticationResult(true, "Usuário cadastrado com sucesso.", input);
         }
+
+        public string HashPasswd(string password)
+        {
+            return HashPassword(password);
+        }
+
+        public bool VerifyPasswd(string passwd, string hash)
+        {
+            return Verify(passwd, hash);
+        }
+
+
     }
 }
